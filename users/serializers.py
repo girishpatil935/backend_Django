f0788bb597b3c433c36213ad36671d1bcca3318f
girishpatil935 <girishoffcial@gmail.com>
2026-05-17 07:46:43 +0530

    serilizer understanding and validation use

--- users/serializers.py
+++ users/serializers.py
@@ -5,3 +5,17 @@ class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
         fields = ['id', 'name', 'user']
+
+def validate_name(self, value):
+
+    if Skill.objects.filter(name=value).exists():
+        raise serializers.ValidationError(
+            "This skill already exists."
+        )
+
+    if len(value) < 3:
+        raise serializers.ValidationError(
+            "Skill name must be at least 3 characters long."
+        )
+
+    return value
