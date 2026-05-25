9557c3eeb8a0ab3de556caf0653ac8032ece404b
girishpatil935 <girishoffcial@gmail.com>
2026-05-25 06:00:26 +0530

    permission and IsOwnership added

--- users/views.py
+++ users/views.py
@@ -12,11 +12,12 @@ from .serializers import RegisterSerializer
 from rest_framework.permissions import IsAuthenticated
 from .models import Task
 from .serializers import TaskSerializer
+from .permissions import IsOwner
 
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
 
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner]
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
