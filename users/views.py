589d145b202a8e1cb7c3f5604c15d8a7fdca9b9d
girishpatil935 <girishoffcial@gmail.com>
2026-06-06 06:09:06 +0530

    Add Docker Compose setup with PostgreSQL

--- users/views.py
+++ users/views.py
@@ -13,7 +13,7 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Task
 from .serializers import TaskSerializer
 from .permissions import IsOwner
-from rest_framework import filters
+from rest_framework import filters 
 
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
